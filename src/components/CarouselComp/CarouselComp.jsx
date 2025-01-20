e0d9cce5adbcc012afa8d1b3f81a5f673170db33
tusherbhakta <tusheranta@gmail.com>
2025-01-20 21:25:16 +0530

    carousel content changed

--- src/components/CarouselComp/CarouselComp.jsx
+++ src/components/CarouselComp/CarouselComp.jsx
@@ -1,35 +1,38 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import educationBanner from "../../assets/education.png";
-import healthBanner from "../../assets/medical.png";
-import startupBanner from "../../assets/startup.png";
+import techInnovationBanner from '../../assets/tech company-rafiki.svg';
+import communityProjectBanner from "../../assets/Community-bro.svg";
+import environmentalCauseBanner from "../../assets/metaverso-bro.svg";
 import { Link } from "react-router-dom";
 
 const CarouselComp = () => {
   const banners = [
     {
-      src: educationBanner,
-      alt: "Transform Lives Through Education",
-      title: "Transform Lives Through Education",
-      description: "Empower underprivileged children with the gift of knowledge. Your support can open doors to opportunities, brighter futures, and lifelong dreams.",
-      linkPrimary: { path: "/add-campaign", text: "Start a Campaign" },
-      linkSecondary: { path: "/campaigns", text: "Discover Campaigns" },
+      src: techInnovationBanner,
+      alt: "Empower Next-Gen Tech Innovators",
+      title: "Empower Next-Gen Tech Innovators",
+      description:
+        "Support groundbreaking tech projects and innovations. Your contribution can shape the future of technology and inspire the next big ideas.",
+      linkPrimary: { path: "/add-campaign", text: "Launch Your Project" },
+      linkSecondary: { path: "/campaigns", text: "Explore Projects" },
     },
     {
-      src: healthBanner,
-      alt: "Be a Lifesaver in Crisis Situation",
-      title: "Be a Lifesaver in Crisis Situation",
-      description: "Extend a helping hand to those facing medical emergencies. Your generosity could mean hope, recovery, and survival for those in need.",
+      src: communityProjectBanner,
+      alt: "Strengthen Communities, Foster Change",
+      title: "Strengthen Communities, Foster Change",
+      description:
+        "Back impactful community-driven initiatives. Together, we can create meaningful change and build stronger, thriving communities.",
       linkPrimary: { path: "/add-campaign", text: "Start a Campaign" },
-      linkSecondary: { path: "/campaigns", text: "Discover Campaigns" },
+      linkSecondary: { path: "/campaigns", text: "View Campaigns" },
     },
     {
-      src: startupBanner,
-      alt: "Fuel Innovation, Fund Startups",
-      title: "Fuel Innovation, Fund Startups",
-      description: "Support visionary entrepreneurs to bring groundbreaking ideas to life. Be part of a journey that redefines industries and shapes the future.",
-      linkPrimary: { path: "/add-campaign", text: "Start a Campaign" },
-      linkSecondary: { path: "/campaigns", text: "Discover Campaigns" },
+      src: environmentalCauseBanner,
+      alt: "Protect the Planet, Fund Sustainability",
+      title: "Protect the Planet, Fund Sustainability",
+      description:
+        "Contribute to environmental conservation and green projects. Join the movement for a cleaner, healthier, and sustainable world.",
+      linkPrimary: { path: "/add-campaign", text: "Initiate Change" },
+      linkSecondary: { path: "/campaigns", text: "Support Causes" },
     },
   ];
 
